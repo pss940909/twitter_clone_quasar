c804864102fdff91cc48a217ba3034617d19db8b
pss940909 <pss940909@gmail.com>
2023-02-28 10:17:53 +0800

    add tweet list

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <div class="q-px-md q-py-lg row">
+    <div class="q-px-md q-py-lg row items-end q-col-gutter-md">
       <div class="col">
         <q-input
           bottom-slots
@@ -26,9 +26,47 @@
           color="primary"
           label="Qweet"
           no-caps
+          class="q-mb-lg"
         />
       </div>
     </div>
+    <q-separator size="10px" color="grey-2" class="divider" />
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Ashley Chen</strong>
+            <span class="text-grey-7">@ashleyChen</span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam,
+            consequuntur aperiam inventore pl aceat totam quae at minus ducimus
+            quisquam laboriosam similique possimus repellat voluptatum
+            cupiditate magnam. Assumenda mollitia quibusdam sapiente.
+          </q-item-label>
+          <div class="row">
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fa-regular fa-comment"
+              size="sm"
+            />
+            <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
+            <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+            <q-btn flat round color="grey" icon="fa-solid fa-trash" size="sm" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -49,5 +87,12 @@ export default defineComponent({
 .new-qweet textarea {
   font-size: 19px;
   line-height: 1.5 !important;
+}
+.divider {
+  border-bottom: 1px solid #f8f8f8;
+  border-top: 1px solid #f8f8f8;
+}
+.qweet-content {
+  white-space: pre-line;
 }
 </style>
