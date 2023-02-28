5453646f5b660910f80c288395ebdb621f184499
pss940909 <pss940909@gmail.com>
2023-02-28 09:39:31 +0800

    first push : New Tweet Form

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex porro alias
+        aperiam, quod maiores exercitationem veritatis architecto deserunt ab
+        praesentium molestiae repellat a repellendus sint harum tempore. Quo,
+        reiciendis repellat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+        exercitationem magni dignissimos sit animi excepturi tenetur laboriosam
+        expedita inventore et quisquam minus, quasi ratione eligendi ipsa quod
+        labore? Voluptatem, facilis.
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageAbout",
+});
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page>
+    <div class="q-px-md q-py-lg row">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweetContent"
+          placeholder="What's happening?"
+          counter
+          autogrow
+          maxlength="280"
+          class="new-qweet"
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          :disable="!newQweetContent"
+          unelevated
+          rounded
+          color="primary"
+          label="Qweet"
+          no-caps
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHome",
+  data() {
+    return {
+      newQweetContent: "",
+    };
+  },
+});
+</script>
+
+<style lang="css">
+.new-qweet textarea {
+  font-size: 19px;
+  line-height: 1.5 !important;
+}
+</style>
