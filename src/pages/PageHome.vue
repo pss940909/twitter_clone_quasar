4033bf0ae1a334299bee562f8934b6f20c02a461
pss940909 <pss940909@gmail.com>
2023-02-28 11:55:42 +0800

    qweet list finished

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -20,6 +20,7 @@
       </div>
       <div class="col col-shrink">
         <q-btn
+          @click="addNewQweet"
           :disable="!newQweetContent"
           unelevated
           rounded
@@ -32,39 +33,53 @@
     </div>
     <q-separator size="10px" color="grey-2" class="divider" />
     <q-list separator>
-      <q-item v-for="qweet in qweets" :key="qweet.date" class="q-py-md">
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-          </q-avatar>
-        </q-item-section>
+      <transition-group
+        appear
+        enter-active-class="animated fadeIn slow"
+        leave-active-class="animated fadeOut slow"
+      >
+        <q-item v-for="qweet in qweets" :key="qweet.date" class="q-py-md">
+          <q-item-section avatar top>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-subtitle1">
-            <strong>Ashley Chen</strong>
-            <span class="text-grey-7">@ashleyChen</span>
-          </q-item-label>
-          <q-item-label class="qweet-content text-body1">
-            {{ qweet.content }}
-          </q-item-label>
-          <div class="qweet-icons row justify-between q-mt-md">
-            <q-btn
-              flat
-              round
-              color="grey"
-              icon="fa-regular fa-comment"
-              size="sm"
-            />
-            <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
-            <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
-            <q-btn flat round color="grey" icon="fa-solid fa-trash" size="sm" />
-          </div>
-        </q-item-section>
-
-        <q-item-section side top>
-          {{ relativeDate(qweet.date) }}
-        </q-item-section>
-      </q-item>
+          <q-item-section>
+            <q-item-label class="text-subtitle1">
+              <strong>Ashley Chen</strong>
+              <span class="text-grey-7">
+                @ashleyChen
+                <br class="lt-sm" />
+                <span class="gt-xs">&bull;</span>
+                {{ relativeDate(qweet.date) }}
+              </span>
+            </q-item-label>
+            <q-item-label class="qweet-content text-body1">
+              {{ qweet.content }}
+            </q-item-label>
+            <div class="qweet-icons row justify-between q-mt-md">
+              <q-btn
+                flat
+                round
+                color="grey"
+                icon="fa-regular fa-comment"
+                size="sm"
+              />
+              <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
+              <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+              <q-btn
+                flat
+                round
+                color="grey"
+                icon="fa-solid fa-trash"
+                size="sm"
+                @click="deleteQweet(qweet)"
+              />
+            </div>
+          </q-item-section>
+        </q-item>
+      </transition-group>
     </q-list>
   </q-page>
 </template>
@@ -80,18 +95,15 @@ export default defineComponent({
       newQweetContent: "",
       qweets: [
         {
-          content:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam ipsum quas, ipsam, perferendis possimus sunt tenetur cumque doloremque maxime, libero consectetur cum voluptas. Assumenda qui quasi ullam. Adipisci, iusto minima.",
+          content: "The best way to predict your future is to create it.",
           date: 1677551116103,
         },
         {
-          content:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam ipsum quas, ipsam, perferendis possimus sunt tenetur cumque doloremque maxime, libero consectetur cum voluptas. Assumenda qui quasi ullam. Adipisci, iusto minima.",
+          content: "Be the change you wish to see in the world.",
           date: 1677551135429,
         },
         {
-          content:
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam ipsum quas, ipsam, perferendis possimus sunt tenetur cumque doloremque maxime, libero consectetur cum voluptas. Assumenda qui quasi ullam. Adipisci, iusto minima.",
+          content: "The only way to do great work is to love what you do.",
           date: 1677551144776,
         },
       ],
@@ -100,6 +112,20 @@ export default defineComponent({
   methods: {
     relativeDate(value) {
       return formatDistance(value, new Date());
+    },
+    addNewQweet() {
+      let newQweet = {
+        content: this.newQweetContent,
+        date: Date.now(),
+      };
+      this.qweets.unshift(newQweet);
+      this.newQweetContent = "";
+    },
+    deleteQweet(qweet) {
+      console.log("qweet", qweet);
+      let dateToDelete = qweet.date;
+      let index = this.qweets.findIndex((qweet) => qweet.date === dateToDelete);
+      this.qweets.splice(index, 1);
     },
   },
 });
